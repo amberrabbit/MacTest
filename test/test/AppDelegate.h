a192c4413cd0d5da31d655119354a475bfe3e7db
amberrabbit <hakrabi1014@gmail.com>
2014-02-16 09:53:03 +0900

    test
    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 大澤亮介 on 2014/02/16.
+//  Copyright (c) 2014年 大澤亮介. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
